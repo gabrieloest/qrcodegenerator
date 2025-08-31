cce9a0eb99fe517fefda394f28fbbd9eec6bfa83
Gabriel Oest <goest@technisys.com>
2025-08-31 20:40:19 -0300

    Add DTOs for request and response for the qrcode post

---
+++ src/main/java/com/oestdev/qrcodegenerator/dto/qrcode/QRCodeRequest.java
@@ -0,0 +1,4 @@
+package com.oestdev.qrcodegenerator.dto.qrcode;
+
+public record QRCodeRequest(String text) {
+}
---
+++ src/main/java/com/oestdev/qrcodegenerator/dto/qrcode/QRCodeResponse.java
@@ -0,0 +1,4 @@
+package com.oestdev.qrcodegenerator.dto.qrcode;
+
+public record QRCodeResponse(String url) {
+}
